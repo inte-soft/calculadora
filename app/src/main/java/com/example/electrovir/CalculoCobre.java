675c31561753f56ee574643eb990fd1167b7da80
DeveloperAlsir <111030332+DeveloperAlsir@users.noreply.github.com>
2022-10-03 06:54:41 -0500

    layout cobre1

---
+++ app/src/main/java/com/example/electrovir/CalculoCobre.java
@@ -0,0 +1,35 @@
+package com.example.electrovir;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class CalculoCobre extends AppCompatActivity {
+
+    private Spinner cobre;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculo_cobre);
+    }
+
+
+    //Arreglo de las lista desplegable cobre.
+    cobre = (Spinner)findViewById(R.id.lcobre);
+    String [] lDatos = {" ","Barras","Laminilla"};
+    ArrayAdapter<String> adaptercobre = new ArrayAdapter<String>(this, R.layout.spinner_item_personalizacion, lDatos);
+    cobre.setAdapter(adaptercobre);
+
+    public void Regresarcobre(View view){
+        Intent regresarcobre1 = new Intent(this,vistaPrincipal.class);
+        startActivity(regresarcobre1);
+        finish();
+    }
+
+
+}
