5b470fa721d73ae442b59513ee1154707c649698
alejo8903 <alejo890327@gmail.com>
2022-12-27 12:13:16 -0500

    actualizacion de la ventilacion con la clase

---
+++ app/src/main/java/intesoft/elecal/elec/ventilacion.java
@@ -0,0 +1,16 @@
+package intesoft.elecal.elec;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.electrovir.R;
+
+public class ventilacion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ventilacion);
+    }
+}
